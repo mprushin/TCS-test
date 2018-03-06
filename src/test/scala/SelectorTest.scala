f20eb08be4570859ebb22afc7b2a707e32e5f0e9
Mikhail Prushinsky (DHL RU) <Mikhail.Prushinsky@dhl.ru>
2018-03-06 12:57:24 +0300

    Added test for empty data

--- src/test/scala/SelectorTest.scala
+++ src/test/scala/SelectorTest.scala
@@ -4,6 +4,8 @@ import org.scalatest.FunSuite
 
 trait SelectorTest extends FunSuite {
 
+  val data0 = List()
+
   val data1 = List((1, new Date(2016, 9, 11)),
     (2, new Date(2016, 9, 12)),
     (3, new Date(2016, 9, 13)),
@@ -22,6 +24,14 @@ trait SelectorTest extends FunSuite {
 
 
   val data2 = ((1 to 43020) zip dateSeq).toList
+
+  test("test Selector1 selection on data0") {
+    assert(Selector1.selectBackDated(data0).sortBy[Int](i => i) == List())
+  }
+
+  test("test Selector2 selection on data0") {
+    assert(Selector2.selectBackDated(data0).sortBy[Int](i => i) == List())
+  }
 
   test("test Selector1 selection on data1") {
     assert(Selector1.selectBackDated(data1).sortBy[Int](i => i) == List(5, 6))
